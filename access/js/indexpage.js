a226cf955994c99672cddf9ff767f55b7515c74e
daso132 <tlucxyn@gmail.com>
2026-03-24 01:13:03 +0700

    first commit

--- access/js/indexpage.js
+++ access/js/indexpage.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const menuBar = document.querySelector(".nav_mobile");
 const menuOverLay = document.querySelector(".overlay_for_mobile_device");
 const menuOpenButton = document.querySelector(".hamberger_bar");
@@ -22,3 +23,30 @@ function closeMenu(){
     menuOverLay.classList.remove("show_overlay");
 }
 
+=======
+const menuBar = document.querySelector(".nav_mobile");
+const menuOverLay = document.querySelector(".overlay_for_mobile_device");
+const menuOpenButton = document.querySelector(".hamberger_bar");
+const body = document.querySelector("body");
+const paralax = document.querySelector(".background_parallax");
+
+window.addEventListener("scroll", () => {
+  let scrollY = window.scrollY;
+  body.style.backgroundPositionY = scrollY * -0.2 + "px";
+  paralax.style.backgroundPositionY = scrollY * -0.4 + "px";
+});
+
+function openMenu(){
+    menuBar.classList.add("show_menu");
+    menuBar.removeAttribute("inert");
+    menuOverLay.classList.add("show_overlay");
+}
+
+function closeMenu(){
+    console.log("close");
+    menuBar.classList.remove("show_menu");
+    menuBar.setAttribute("inert","");
+    menuOverLay.classList.remove("show_overlay");
+}
+
+>>>>>>> f05ca4b (first commit)
--- access/js/prj1.js
+++ access/js/prj1.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const menuBar = document.querySelector(".nav_mobile");
 const menuOverLay = document.getElementById("overlay_for_mobile_device");
 const menuOpenButton = document.querySelector(".hamberger_bar");
@@ -9,3 +10,16 @@ function closeMenu(){
     menuBar.classList.remove("show_menu");
 }
 
+=======
+const menuBar = document.querySelector(".nav_mobile");
+const menuOverLay = document.getElementById("overlay_for_mobile_device");
+const menuOpenButton = document.querySelector(".hamberger_bar");
+function openMenu(){
+    menuBar.classList.add("show_menu");
+}
+
+function closeMenu(){
+    menuBar.classList.remove("show_menu");
+}
+
+>>>>>>> f05ca4b (first commit)
